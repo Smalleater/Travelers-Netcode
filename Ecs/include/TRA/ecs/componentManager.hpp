ecf0c410772e2e1e093ac08e7713a3e3d3edfc06
Adrien Claude <163728051+Smalleater@users.noreply.github.com>
2025-12-14 11:41:40 +0100

    Add getOrCreateComponentStore to ComponentManager
    Introduced a template method to retrieve or create component stores using shared pointers, replacing unique_ptr with shared_ptr for m_sparseSets. This change improves component store management and sharing within the ECS framework.

--- Ecs/include/TRA/ecs/componentManager.hpp
+++ Ecs/include/TRA/ecs/componentManager.hpp
@@ -12,7 +12,23 @@ namespace tra::ecs
 		~ComponentManager() = default;
 
 	private:
-		std::unordered_map<size_t, std::unique_ptr<ISparseSet>> m_sparseSets;
+		std::unordered_map<size_t, std::shared_ptr<ISparseSet>> m_sparseSets;
+
+		template<typename Component>
+		std::weak_ptr<SparseSet<Component>> getOrCreateComponentStore()
+		{
+			static_assert(std::is_base_of<IComponent, Component>::value, "Ecs: Component must derive from IComponent");
+
+			size_t hashCode = typeid(Component).hash_code();
+			auto it = m_sparseSets.find(hashCode);
+			if (it != m_sparseSets.end())
+			{
+				return std::static_pointer_cast<SparseSet<Component>>(it->second);
+			}
+
+			m_sparseSets[hashCode] = std::make_shared<SparseSet<Component>>();
+			return std::static_pointer_cast<SparseSet<Component>>(m_sparseSets[hashCode]);
+		}
 	};
 }
 
