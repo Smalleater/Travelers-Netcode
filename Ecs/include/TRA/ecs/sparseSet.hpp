219385dea750b94dfbf6c53e4f717f21bb2214f0
Adrien Claude <163728051+Smalleater@users.noreply.github.com>
2025-12-15 19:51:58 +0100

    Add remove, get, and hasComponent to sparseSet
    Implemented remove, get, and hasComponent methods for the sparseSet class. Also added m_denseToEntity vector to efficiently map dense indices to entities, improving component management and lookup.

--- Ecs/include/TRA/ecs/sparseSet.hpp
+++ Ecs/include/TRA/ecs/sparseSet.hpp
@@ -43,10 +43,49 @@ namespace tra::ecs
 
 			m_sparse[_entity] = m_dense.size();
 			m_dense.emplace_back(_component);
+			m_denseToEntity.push_back(_entity);
+		}
+
+		void remove(const Entity& _entity)
+		{
+			auto it = m_sparse.find();
+			if (it != m_sparse.end())
+			{
+				size_t denseIndex = it->second;
+				size_t lastDenseIndex = m_dense.size() - 1;
+
+				if (denseIndex != lastDenseIndex)
+				{
+					m_dense[denseIndex] = m_dense[lastDenseIndex];
+					m_denseToEntity[denseIndex] = m_denseToEntity[lastDenseIndex];
+					m_sparse[m_denseToEntity[denseIndex]] = denseIndex;
+				}
+
+				m_dense.pop_back();
+				m_denseToEntity.pop_back();
+				m_sparse.erase(it);
+			}
+		}
+
+		std::weak_ptr<Component> get(Entity _entity)
+		{
+			auto it = m_sparse.find(_entity);
+			if (it == m_sparse.end())
+			{
+				return nullptr;
+			}
+
+			return m_dense[it->second];
+		}
+
+		bool hasComponent(EntityId _entity)
+		{
+			return m_sparse.find(_entity) != m_sparse.end();
 		}
 
 	private:
 		std::vector<Component> m_dense;
+		std::vector<Entity> m_denseToEntity;
 		std::unordered_map<Entity, size_t> m_sparse;
 	};
 }
